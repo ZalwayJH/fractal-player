a089c402d8bc772daf23b98d9deace5ff5179405
ZalwayJH <joelhaughton94@gmail.com>
2024-04-14 15:18:48 +0100

    changed audio control css, the original indicator is no invisible and is used as a hover to reveal the actual audio controls, also added a song to local file paths for testing

--- src/renderer/src/components/menus/AudioControls.jsx
+++ src/renderer/src/components/menus/AudioControls.jsx
@@ -1,18 +1,21 @@
 import React, { useState } from 'react';
+import audioSrc from '../../songs/audoMoe.mp3';
 
 function AudioControls() {
-  const [isOpen, setIsOpen] = useState('hidden');
   return (
-    <div className="group">
-      <div
-        className={`z-10 right-5 left-5 bottom-5 rounded-md group-hover:hidden mx-auto min-w-5 w-3/6 h-3 backdrop-blur-sm bg-white/60 absolute group-hover:$ `}
-      ></div>
-      <audio
-        src="https://www.zapsplat.com/wp-content/uploads/2015/sound-effects-61905/zapsplat_multimedia_alert_chime_short_musical_notification_cute_child_like_001_64918.mp3?_=1"
-        className={`z-10   right-5 left-5 bottom-5 mx-auto min-w-5 w-3/6 absolute opacity-0 group-hover:opacity-100 `}
-        controls
-        controlsList="nodownload noplaybackrate"
-      ></audio>
+    <div>
+      <div className="group ease-in-out duration-300">
+        <div
+          className={`z-10 right-5 left-5 bottom-0  rounded-md mx-auto h-6 min-w-min absolute group-hover:opacity-100 `}
+        >
+          <audio
+            src={audioSrc}
+            className={`z-10 bottom-0 right-5 left-5 group-hover:bottom-5 mx-auto min-w-5 w-3/6 absolute ease-out duration-300 transition-all opacity-0 group-hover:opacity-100  `}
+            controls
+            controlsList="nodownload noplaybackrate"
+          ></audio>
+        </div>
+      </div>
     </div>
   );
 }
