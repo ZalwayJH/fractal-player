25feea97ef44c431e2c1d140697da53e32a297c6
ZalwayJH <joelhaughton94@gmail.com>
2024-06-05 22:09:35 +0100

    made some alterations to scrollbar and changed titlebar to position:fixed instead of absolute

--- src/renderer/src/components/TitleBar/TitleBar.jsx
+++ src/renderer/src/components/TitleBar/TitleBar.jsx
@@ -5,25 +5,25 @@ function TitleBar() {
   return (
     <div
       id="titleBar"
-      className=" z-20 w-full bg-black/0 grid grid-flow-col h-[1em] justify-end gap-5 auto-cols-max absolute m-0 top-0 p-3 "
+      className="fixed z-20 w-full bg-black/0 grid grid-flow-col h-[1em] justify-end gap-5 auto-cols-max m-0 top-0 p-3 "
     >
       <button
         id="titleButtonsMin"
-        className=" rounded-xl p-2 z-30  backdrop-blur-xl  bg-white/[0.1]  hover:bg-[#4fd6be]/[1] ring-white/[0.2] ring-1 hover:shadow-[0px_0px_10px_1px_#4fd6be] "
+        className="rounded-xl p-2 z-30  backdrop-blur-xl  bg-white/[0.1]  hover:bg-[#4fd6be]/[1] ring-white/[0.2] ring-1 hover:shadow-[0px_0px_10px_1px_#4fd6be] "
         onClick={() => {
           minimizeWindow('minimize-window');
         }}
       ></button>
       <button
         id="titleButtonsMax"
-        className="rounded-xl p-2 z-30   backdrop-blur-xl bg-white/[0.1] hover:bg-[#7aa2f7]/[1]  ring-white/[0.2] ring-1 hover:shadow-[0px_0px_10px_1px_#7aa2f7]"
+        className=" rounded-xl p-2 z-30   backdrop-blur-xl bg-white/[0.1] hover:bg-[#7aa2f7]/[1]  ring-white/[0.2] ring-1 hover:shadow-[0px_0px_10px_1px_#7aa2f7]"
         onClick={() => {
           maximizeWindow('maximize-window');
         }}
       ></button>
       <button
         id="titleButtonsClose"
-        className="rounded-xl z-30 p-2 backdrop-blur-xl bg-white/[0.1]  hover:bg-rose-500/[1] ring-white/[0.2] ring-1 hover:shadow-[0px_0px_10px_1px_#c53b53]"
+        className=" rounded-xl z-30 p-2 backdrop-blur-xl bg-white/[0.1]  hover:bg-rose-500/[1] ring-white/[0.2] ring-1 hover:shadow-[0px_0px_10px_1px_#c53b53]"
         onClick={() => {
           closeWindow('close-window');
         }}
